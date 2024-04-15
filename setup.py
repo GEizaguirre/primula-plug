3cda3040fd1d62a85a41f251fa0e06d455452815
german <gteizaguirre@gmail.com>
2024-04-15 23:55:51 +0200

    setup.py corrected

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
 import os
+import pathlib
 
+from setuptools import find_packages
+
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(name='primula',
       version='1.0',
       description='The Primula algorithm, as a generic pluggable predictor. ',
-      author='Germán Telmo Eizaguirre',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Germán T. Eizaguirre',
       author_email='germantelmo.eizaguirre@urv.cat',
       url='https://github.com/GEizaguirre/primula-plug',
-      packages=['primula']
+      packages=find_packages()
      )
 
 home_dir = os.path.expanduser("~")
